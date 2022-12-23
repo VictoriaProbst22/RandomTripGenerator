4c89b5b37b730604700268ff4d9972255806de12
Victoria Probst <victoria.probst13@gmail.com>
2022-12-23 17:52:20 -0500

    Updated if statement comparing User Input to Choices

--- Program.cs
+++ Program.cs
@@ -9,6 +9,13 @@ int index = rand.Next(destinations.Length);
 
 
 Console.WriteLine("Hello, Welcome to Your Random Day Trip Generator! Please Choose a Destination!");
-Console.WriteLine($"Your First Random Destination Chosen for you is {destinations[index]}, Would You Like A Different Place?(Y/N)");
+Console.WriteLine($"Your First Random Destination Chosen for you is {destinations[index]}, Do You Like This Place?(Y/N)");
 string inputChoice = Console.ReadLine();
 Console.WriteLine($"You Chose {inputChoice}!");
+if (inputChoice == "Y")
+{
+    Console.WriteLine("Lets See What Snacks They Have While Your There!");
+} else 
+{
+   Console.WriteLine($"How About {destinations[index]}?");
+}
