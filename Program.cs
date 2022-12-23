e5409a7df0c86c74ec7c95c33724dd2f5d98d86e
Victoria Probst <victoria.probst13@gmail.com>
2022-12-23 18:22:11 -0500

    Finished Destinations Random Choice Options with User Input

--- Program.cs
+++ Program.cs
@@ -12,10 +12,37 @@ Console.WriteLine("Hello, Welcome to Your Random Day Trip Generator! Please Choo
 Console.WriteLine($"Your First Random Destination Chosen for you is {destinations[index]}, Do You Like This Place?(Y/N)");
 string inputChoice = Console.ReadLine();
 Console.WriteLine($"You Chose {inputChoice}!");
-if (inputChoice == "Y")
+switch(inputChoice) 
 {
-    Console.WriteLine("Lets See What Snacks They Have While Your There!");
-} else 
-{
-   Console.WriteLine($"How About {destinations[index]}?");
+  case "Y":
+    Console.WriteLine("Great Choice! Lets See What Snacks Are There!");
+    break;
+  case "N":
+    Console.WriteLine($"Your Next Choice is {destinations[index]}, Good Choice? (Y/N)");
+    string inputChoice2 = Console.ReadLine();
+    Console.WriteLine($"You chose {inputChoice2}");
+    if(inputChoice2 == "Y")
+    {
+        Console.WriteLine("You Choose The Best Spot! Lets See What Food Options You Have There! ");
+    } else if (inputChoice2 == "N")
+    {
+        Console.WriteLine($"Your Next Option Is: {destinations[index]}, Is This Your Choice?");
+        string inputChoice3 = Console.ReadLine();
+        Console.WriteLine($"You Chose {inputChoice3}!");
+        if (inputChoice3 == "Y")
+        {
+            Console.WriteLine("Great Lets Move Along!");
+        } else
+        {
+            Console.WriteLine("I have No Other Options :( , Lets Start Over!");
+        }
+
+    } else
+    {
+        Console.WriteLine($"Your Last Option Is: {destinations[index]}");
+    }
+    break;
+  default:
+    Console.WriteLine("Thats not a choice! Try Again!");
+    break;
 }
