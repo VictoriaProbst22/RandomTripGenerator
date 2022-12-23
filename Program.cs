f209d7da5949b4600e6941996dd2127a80db7837
Victoria Probst <victoria.probst13@gmail.com>
2022-12-23 17:47:41 -0500

    Established Destinations Array and Gathered User Input for Welcome Message and Destination Options

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//Destinations Array
+string[] destinations = {"Reptar World", "Glove World", "Atlantis", "Inner Earth"};
+
+Random rand = new Random();
+int index = rand.Next(destinations.Length);
+
+
+
+Console.WriteLine("Hello, Welcome to Your Random Day Trip Generator! Please Choose a Destination!");
+Console.WriteLine($"Your First Random Destination Chosen for you is {destinations[index]}, Would You Like A Different Place?(Y/N)");
+string inputChoice = Console.ReadLine();
+Console.WriteLine($"You Chose {inputChoice}!");
